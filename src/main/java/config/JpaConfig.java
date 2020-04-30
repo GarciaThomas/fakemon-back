f54a636073eca04d6ba3b29d15d2e02947f94828
Thomas <monsieurgt@gmail.com>
2020-04-30 10:16:54 +0200

    Configuration Spring JPA

---
+++ src/main/java/config/JpaConfig.java
@@ -0,0 +1,52 @@
+package config;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("dao")
+public class JpaConfig {
+	
+	@Bean public LocalContainerEntityManagerFactoryBean entityManagerFactory(BasicDataSource dataSource) {
+		System.out.println("Chargement");
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		emf.setDataSource(dataSource); 
+		emf.setPackagesToScan("model"); 
+		emf.setJpaVendorAdapter(vendorAdapter); 
+		emf.setJpaProperties(this.hibernateProperties());
+		return emf;
+	}
+	
+	private Properties hibernateProperties() { 
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update"); 
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+		properties.setProperty("hibernate.show_sql", "true"); 
+		properties.setProperty("hibernate.format_sql", "true");
+		return properties;
+	}
+	
+	@Bean public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf); return transactionManager;
+	}
+	
+	@Bean public PersistenceExceptionTranslationPostProcessor exceptionTranslation() { 
+		return new PersistenceExceptionTranslationPostProcessor(); 
+	}
+
+}
