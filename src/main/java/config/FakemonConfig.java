5e5c3bea7942074876ceec3e0edb42177696aa5a
PPJJ <ppjj.fr@gmail.com>
2020-05-07 10:14:08 +0200

    Rajout du fichier qu'il a supprimé sans raison apparente....

---
+++ src/main/java/config/FakemonConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"config","controller","service","model"})
+public class FakemonConfig {
+
+		@Bean
+		public BasicDataSource dataSource(){
+			BasicDataSource dataSource = new BasicDataSource();
+			dataSource.setDriverClassName("com.mysql.jdbc.Driver"); 
+			dataSource.setUrl("jdbc:mysql://localhost:3306/fakemon"); 
+			dataSource.setUsername("root"); 
+			dataSource.setPassword(""); 
+			dataSource.setMaxTotal(10);
+			return dataSource;
+		}
+
+	
+}
